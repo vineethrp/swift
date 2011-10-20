c062d4bad5fd8aa4a7bc5ad481b23c07216f281a
Vineeth Pillai <vineethrp@gmail.com>
2011-10-20 10:18:17 -0700

    Add LFS(Local File System) capability to swift so that swift supports different filesystems with varying capabilities. Support for ZFS also included.

---
+++ swift/common/lfs/lfs.py
@@ -0,0 +1,81 @@
+# Copyright (c) 2010-2011 OpenStack, LLC.
+# Copyright 2011 Nexenta Systems Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import time
+from threading import Thread
+from swift.common.utils import mkdirs
+import os
+
+class LFS(object):
+    def __init__(self, conf, srvdir):
+        self.conf = conf
+        self.root = conf.get('devices', '/srv/node')
+        self.srvdir = srvdir
+
+    def setup_partition(self, device, partition):
+        path = os.path.join(self.root, device, self.srvdir, partition)
+        mkdirs(path)
+
+    def setup_objdir(self, device, partition, name_hash):
+        # Creation of objdir is done by object server by default.
+        pass
+
+    def tmp_dir(self, device, partition, name_hash):
+        return os.path.join(self.root, device, self.srvdir, 'tmp')
+
+class LFSDefault(LFS):
+    def __init__(self, conf, srvdir):
+        LFS.__init__(self, conf, srvdir)
+        self.fs = "xfs"
+
+
+class LFSStatus(Thread):
+    """
+    Status Checker thread which checks the status of filesystem and
+    calls back to LFS if it sees any issues.
+    This thread periodically calls the check_func, if the filesystem is
+    healthy. If check_func returns non-zero, it temporarly stops checking
+    until the fault is cleared.
+
+    interval        :   interval in seconds for checking FS
+    check_func      :   method for checking FS. Takes exactly one argument
+                        which should be a tuple. Returns 0 if FS is healthy
+    check_args      :   tuple argument to check_func
+    """
+
+    def __init__(self, interval, check_func, check_args):
+        Thread.__init__(self)
+        self.interval = interval
+        self.check_func = check_func
+        self.check_args = check_args
+        self.faulty = False
+        self.daemon = True
+
+    def run(self):
+        while True:
+            if not self.faulty:
+                ret = self.check_func(self.check_args)
+                if ret != None:
+                    # ret must be a tuple (<callback function>, <args>)
+                    self.faulty = True
+                    ret[0](ret[1])
+
+            time.sleep(self.interval)
+
+    # Clears the fault, so that status check thread can resume.
+    def clear_fault(self):
+        self.faulty = False
+
